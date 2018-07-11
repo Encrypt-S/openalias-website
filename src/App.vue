736402d1e0a4d470819fa9f97a6ccca5caf94f21
Matt <mpau548@aucklanduni.ac.nz>
2018-07-11 16:45:54 +0200

    checking in cahgnes

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js Test App"/> -->
-    <OpenAlias/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import OpenAlias from './components/OpenAlias.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    OpenAlias
-  }
-}
-</script>
 
 <style>
 #app {
---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img src="../assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <input type="text" placeholder="enter text you want to display on the next page" v-model="text">
+    <button @click="sendData(text)">Click me to push data to the other page</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    sendData: function(text) {
+      this.$router.push({ name: 'stepOne', params: { incomingData: text } })
+    }
+  },
+  data: function() {
+    return {
+      text: ''
+    }
+  }
+}
+</script>
