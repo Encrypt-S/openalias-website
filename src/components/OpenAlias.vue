5d67af7bf65f1c86b58e94be3bc1e99323aafaa2
Matt <mpau548@aucklanduni.ac.nz>
2018-07-11 14:53:21 +0200

    initial commit

---
+++ src/components/OpenAlias.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <p>
+      Look I'm rendering things
+    </p>
+    <input  @input="checkAddress" type="text" v-model="msg">
+    <h1>{{ msg }}</h1>
+    <p>{{ error }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OpenAlias',
+  props: {
+    msg: String,
+    error: String
+  },
+  methods: {
+    checkAddress: function() {
+      if (this.msg.length < 25) {
+        this.error = 'Address too short'
+      } else 
+        this.error = ''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
